4fc9952586afd6dbc7e73d5e21bc07b5dc27772f
shd188 <zs.shd15@gmail.com>
2016-01-25 19:11:24 +0800

    Initial commit

---
+++ app/src/main/java/com/aimer/shd/gank/MyApplication.java
@@ -0,0 +1,26 @@
+package com.aimer.shd.gank;
+
+import android.app.Application;
+
+import com.orhanobut.logger.AndroidLogTool;
+import com.orhanobut.logger.LogLevel;
+import com.orhanobut.logger.Logger;
+
+
+/**
+ * Created by shd on 2016/1/25.
+ */
+public class MyApplication extends Application {
+
+    private static final String TAG="Gank";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Logger.init(TAG)                 // default PRETTYLOGGER or use just init()
+                .methodCount(3)                 // default 2
+                .hideThreadInfo()               // default shown
+                .logLevel(LogLevel.FULL)        // default LogLevel.FULL
+                .methodOffset(2)                // default 0
+                .logTool(new AndroidLogTool()); // custom log tool, optional
+    }
+}
